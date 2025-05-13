3b28e29d09f950521ad64bed39073865e49149b1
Gaana-Shree-S <eng22am0014@dsu.edu.in>
2025-05-13 21:47:20 +0530

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './header.css'
+export default function Header() {
+  return (
+    <div>
+                        <img src='./igk.png' style={{height:'90px'}}></img>
+
+      <ul>
+        <li>Search</li>
+        <li><b>India</b></li>
+        <li><b>World</b></li>
+        <li><b>Movies</b></li>
+        <li><b>Sport</b></li>
+        <li><b>Data</b></li>
+        <li><b>Health</b></li>
+        <li><b>Opinion</b></li>
+        <li><b>Science</b></li>
+        <li><b>Business</b></li>
+        <li><b>Premium</b></li>
+      </ul>
+    </div>
+  )
+}
---
+++ src/onemore.js
@@ -0,0 +1,30 @@
+// import React, { useState, useEffect } from 'react'
+// import { motion } from 'framer-motion'
+//  function onemore1() {
+//   return (
+//      <div>
+//         {question.options.map((option, index) => (
+//           <motion.button
+//             key={index}
+//             whileHover={{ scale: 1.05 }}
+//             whileTap={{ scale: 0.95 }}
+//             className={`w-full p-2 rounded ${
+//               answered
+//                 ? option === question.correctAnswer
+//                   ? 'bg-[#6F758B]'
+//                 : 'bg-black'
+//               : 'bg-[#6F758B ] hover:bg-[#6F758B]'
+//             } transition-colors duration-300`}
+//             onClick={() => !answered && onAnswer(option === question.correctAnswer)}
+//             disabled={answered}
+//           >
+//             {option}
+//           </motion.button>
+//         ))}
+//       </div>
+//   )
+// }
+
+   
+
+//       export default onemore
